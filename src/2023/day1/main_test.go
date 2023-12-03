b3dad4e0f381a4c2a7463c9fa0e6f8ca6e6e5aa1
TobinPalmer <tobinkpalmer@gmail.com>
2023-12-02 20:53:57 -0800

    Finished day 1

---
+++ src/2023/day1/main_test.go
@@ -0,0 +1,63 @@
+package main
+
+import "testing"
+
+var example1 = `one2ajsfjas4`
+var example2 = `27sixhpg1fivefxfjxtpggzlqdgnv`
+
+func Test_part1(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{
+			name:  "example",
+			input: example1,
+			want:  24,
+		},
+		{
+			name:  "actual",
+			input: input,
+			want:  54601,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part1(tt.input); got != tt.want {
+				t.Errorf("part1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_part2(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{
+			name:  "example",
+			input: example2,
+			want:  25,
+		},
+		{
+			name:  "example2",
+			input: "oajjsonefojdafiojo234jfoasdfji",
+			want:  14,
+		},
+		// {
+		// 	name:  "actual",
+		// 	input: input,
+		// 	want:  0,
+		// },
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part2(tt.input); got != tt.want {
+				t.Errorf("part2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
